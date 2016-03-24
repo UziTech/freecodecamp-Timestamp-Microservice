6ba004755ecbfb896544e8ead6ddceb7e8e89e80
Tony Brix <tbrix13@uzitech.com>
2016-03-24 10:55:55 -0500

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var moment = require('moment');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(function (req, res) {
+	var input = decodeURIComponent(req.path.replace(/^\//, ""));
+	var time = {unix: null, natural: null};
+	var date;
+	if (!input.match(/\D/)) {
+		date = moment(input, "X");
+	} else {
+		date = moment(input, "MMMM D, YYYY");
+		if (date.format("MMMM D, YYYY") !== input) {
+			date = moment.invalid();
+		}
+	}
+	if (date.isValid()) {
+		time.unix = date.unix();
+		time.natural = date.format("MMMM D, YYYY");
+	}
+	res.json(time);
+});
+
+app.listen(app.get('port'), function () {
+	console.log('Node app is running on port', app.get('port'));
+});
